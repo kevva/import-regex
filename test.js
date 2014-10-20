5409d4f2095b9734cf6b2027fae6521d74b59e32
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-20 23:33:27 +0200

    Use tabs instead of spaces

--- test.js
+++ test.js
@@ -4,16 +4,16 @@ var importRegex = require('./');
 var test = require('ava');
 
 test('match CSS @imports', function (t) {
-    t.plan(3);
+	t.plan(3);
 
-    var fixtures = [
-        '@import url(\'foo.css\');',
-        '@import url(\'bar.css\');'
-    ];
+	var fixtures = [
+		'@import url(\'foo.css\');',
+		'@import url(\'bar.css\');'
+	];
 
-    fixtures.forEach(function (el) {
-        t.assert(importRegex().test(el), el);
-    });
+	fixtures.forEach(function (el) {
+		t.assert(importRegex().test(el), el);
+	});
 
-    t.assert(importRegex().exec('aaa @import url(\'foo.css\'); aaa ')[0].trim() === '@import url(\'foo.css\')');
+	t.assert(importRegex().exec('aaa @import url(\'foo.css\'); aaa ')[0].trim() === '@import url(\'foo.css\')');
 });
