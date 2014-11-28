9cb95893d1be6dc52b449fa84658a489344b3d23
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-28 16:20:03 +0100

    Remove line break

--- index.js
+++ index.js
@@ -9,7 +9,6 @@
 
 module.exports = function (opts) {
 	opts = opts || {};
-
 	var regex = '(?:@import)(?:\\s)(?:url)?(?:(?:(?:\\()(["\'])?(?:[^"\')]+)\\1(?:\\))|(["\'])(?:.+)\\2)(?:[A-Z\\s])*)+(?:;)';
 
 	return opts.exact ? new RegExp('(?:^' + regex + '$)', 'i') :
